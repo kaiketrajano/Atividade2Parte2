00875ed529f126403893d6dcdcf241ea1f2afa57
Kaike Trajano <kaiketrajano48@gmail.com>
2025-11-11 15:06:15 -0300

    Add files via upload

--- src/questao5.js
+++ src/questao5.js
@@ -7,11 +7,17 @@ const pedidosJSON = `{
 }`;
 
 function resumoPedidos(jsonString) {
-  const obj = 
-JSON.parse(jsonString);
-  const entregues:
-obj.pedidos.filter(p => p.status === "entregue").length
-obj.pedidos.filter(p => p.status === "processando").length
-  const total =
+  const obj = JSON.parse(jsonString);
+  const entregues = obj.pedidos.filter((p) => p.status === 'entregue').length;
+  const processamento = obj.pedidos.filter(
+    (p) => p.status === 'processando',
+  ).length;
+  const total = obj.pedidos.reduce((acc, p) => acc + p.total, 0);
 
+  return `
+  ${entregues} pedidos entregues, ${processamento} em processamento. Valor total: R$ ${total.toFixed(
+    2,
+  )}`;
 }
+
+console.log(resumoPedidos(pedidosJSON));
